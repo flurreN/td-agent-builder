044ad956f81e3ab876dff9dedf4132a3b328ebab
Masahiro Nakagawa <repeatedly@gmail.com>
2020-04-08 15:00:45 +0900

    update dependent gems

--- td-agent/core_gems.rb
+++ td-agent/core_gems.rb
@@ -1,20 +1,17 @@
 dir 'core_gems'
-download "bundler", "1.16.6"
+
+download "bundler", "2.1.4"
 download "msgpack", "1.3.3"
-if td_agent_2?
-  download "cool.io", "1.5.1"
-  download "oj", "2.18.5"
-else
-  download "cool.io", "1.6.0"
-  download 'serverengine', '2.2.1'
-  download "oj", "3.8.1"
-  download "async-http", "0.50.7"
-end
+download "cool.io", "1.6.0"
+download 'serverengine', '2.2.1'
+download "oj", "3.10.6"
+download "async-http", "0.50.13"
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.4.1"
 download "sigdump", "0.2.4"
-download "tzinfo", "2.0.1"
+download "tzinfo", "2.0.2"
 download "tzinfo-data", "1.2019.3"
+
 if windows?
   download 'ffi', '1.12.2'
   download 'ffi-win32-extensions', '1.0.3'
